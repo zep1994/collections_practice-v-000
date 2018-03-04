aab7e15f81d4277e39a4dab37898279b4090229b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-04 20:21:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort do |arr|
+    1 <=> 2
+  end
+end
 
+def sort_array_desc
+  
+end
+
+def sort_array_char
+  
+end
+
+def swap_elements
+  
+end
+
+def reverse_array
+  
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
